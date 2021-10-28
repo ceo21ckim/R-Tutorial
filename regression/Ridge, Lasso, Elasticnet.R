d883645f0d12cf94ac8ec53ce42d4e563c256058
KIM DONG EON <60685175+ceo21ckim@users.noreply.github.com>
2021-10-28 20:17:39 +0900

    Add files via upload

--- regression/Ridge, Lasso, Elasticnet.R
+++ regression/Ridge, Lasso, Elasticnet.R
@@ -2,28 +2,36 @@
 # y : medv(주택가격)
 # x : crim, zn, indus, chas, nox, rm, age, dis, rad, tax, ptratio, black, lstat (13th variable)
 
-str(Boston)
 
 # caret ----
 # createDataPartition ----
+
+# createDataPartition : 데이터를 훈련 데이터와 테스트 데이터로 분할
+# createResample : 부트스트래핑을 사용한 샘플링
+# createFolds, createMultiFolds : 교차 검증을 원할 때
+# 데이터를 분할할 때에는 y값의 각 비율이 동일하게 적용된다.
 
 if (!require('glmnet')) install.packages('glmnet')
 if (!require('caret')) install.packages('caret')
 
 library(MASS);library(caret); library(glmnet) ; set.seed(42)
-
+rm(list = ls()) ;str(Boston)
 # times : the number of partitions to create
 # p : the percentage of data that goes to training
-train <- createDataPartition(y=Boston$medv, p = 0.7, list = FALSE)
+# createDataPartition(y, times = 1, p = 0.5, list = TRUE, groups = min(5, length(y)))
 
+train <- createDataPartition(y=Boston$medv,times = 1, p = 0.7, list = FALSE)
+Boston
+
+# target : crim
 Boston.train <- Boston[train,] ; Boston.test <- Boston[-train,]
 
 x <- model.matrix(medv ~., Boston.train)[,-1] # 절편 부분을 제거.
 y <- Boston.train$medv
 
+# Ridge ----
 # alpha = 0 : Ridge, alpha = 1 : Lasso
 # family  = 'gaussian', 'poisson', 'binomial', 'multinomial'
-# Ridge ----
 Boston.cv <- cv.glmnet(x=x, y=y, family='gaussian', alpha = 0 )
 
 plot(Boston.cv)
@@ -38,16 +46,18 @@ log(Boston.cv$lambda.min)
 
 
 Boston.gnet <- glmnet(x, y, family = 'gaussian', alpha = 0, lambda = Boston.cv$lambda.min)
+
 coef(Boston.gnet)
 
 Boston.test.x <- model.matrix(medv ~., Boston.test)[,-1]
 Boston.pred <- predict(Boston.gnet, newx=Boston.test.x)
 postResample(pred=Boston.pred, obs=Boston.test$medv)
 
-
 # Lasso ----
 set.seed(42)
 Boston.cv <- cv.glmnet(x=x, y=y, family='gaussian', alpha=1)
+
+
 
 Boston.cv$lambda.min
 # 0.02536255
@@ -78,6 +88,8 @@ Boston.gnet2 <- glmnet(x, y, family = 'gaussian',
                        alpha = 1, lambda = Boston.cv$lambda.1se)
 
 Boston.pred2 <- predict(Boston.gnet2, newx=Boston.test.x)
+
+# 평가 코드
 postResample(pred=Boston.pred2, obs=Boston.test$medv)
 
 
