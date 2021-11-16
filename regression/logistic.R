380f4db4bcbeafcb6a4bad31f2d9b02f834f3d75
KIM DONG EON <60685175+ceo21ckim@users.noreply.github.com>
2021-11-16 22:06:38 +0900

    Add files via upload

--- regression/logistic.R
+++ regression/logistic.R
@@ -125,3 +125,73 @@ fit.overdis <- glm(formula = churn ~ international_plan + voice_mail_plan +
 pchisq(summary(fit.overdis)$dispersion * fit.origin$df.residual,
        fit.origin$df.residual, lower.tail = FALSE)
 
+
+# p240 ----
+# penalty logistic regression ----
+# !if (require('mlbench')) install.packages('mlbench')
+
+library(mlbench)
+
+# 환자의 당뇨병 여부 와 관련 임상 데이터가 저장된 라이브러리.
+data(PimaIndiansDiabetes2)
+str(PimaIndiansDiabetes2)
+
+# 결측치 제거
+PimaIndiansDiabetes3 <- na.omit(PimaIndiansDiabetes2)
+
+library(caret)
+set.seed(123)
+
+# 7:3 으로 분할
+train <- createDataPartition(y = PimaIndiansDiabetes3$diabetes, p = 0.7, list = FALSE)
+
+diabete.train <- PimaIndiansDiabetes3[train,]
+diabete.test <- PimaIndiansDiabetes3[-train,]
+
+x <- model.matrix(diabetes ~., diabete.train)[,-1]
+# 로지스틱 모델에 적용하기 위해서 char data를 numeric으로 변환
+y <- ifelse(diabete.train$diabetes == 'pos', 1, 0)
+
+library(glmnet)
+
+set.seed(123)
+
+# alpha = 1 : 라쏘회귀분석, alpha = 1 : 릿지회귀분석
+diabete.cv <- cv.glmnet(x = x, y = y, family = 'binomial', alpha = 1)
+
+diabete.cv$lambda.min
+
+diabete.cv$lambda.1se
+
+coef(diabete.cv, diabete.cv$lambda.min)
+
+coef(diabete.cv, diabete.cv$lambda.1se)
+
+# 예측정확도와 간명도는 서로 트레이드오프 관계에 있음.
+
+diabete.gnet1 <- glmnet(x, y, family = 'binomial',
+                        alpha = 1, lambda = diabete.cv$lambda.min)
+
+diabete.test.x <- model.matrix(diabetes ~., diabete.test)[,-1]
+diabete.pred1 <- predict(diabete.gnet1, newx=diabete.test.x, type = 'response')
+diabete.pred1 <- ifelse(diabete.pred1 > 0.5, 'pos', 'neg')
+
+# confusion matrix ----
+table(diabete.test$diabetes, diabete.pred1, dnn = c("Actual", 'Predicted'))
+
+mean(diabete.pred1==diabete.test$diabetes)
+
+diabete.gnet2 <- glmnet(x, y, family = 'binomial',
+                        alpha = 1, lambda = diabete.cv$lambda.1se)
+
+diabete.test.x <- model.matrix(diabetes ~., diabete.test)[,-1]
+diabete.pred2 <- predict(diabete.gnet2, newx=diabete.test.x, type = 'response')
+diabete.pred2 <- ifelse(diabete.pred2 > 0.5, 'pos', 'neg')
+
+table(diabete.test$diabetes, diabete.pred2, dnn = c("Actual", 'Predicted'))
+
+mean(diabete.pred2==diabete.test$diabetes)
+
+
+# multinomial logistic regression ----
+
